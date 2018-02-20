c578c52e70c4fe438781ef66a77b04e2e3aa80c5
Florian Ewald <wuwu@gmx.net>
2018-02-20 17:46:48 +0100

    initial commit

---
+++ config/SiteConfig.js
@@ -0,0 +1,48 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Emilia', // Navigation and Site Title
+  siteTitleAlt: 'Emilia - Gatsby Starter Portfolio', // Alternative Site title for SEO
+  siteUrl: 'https://portfolio-emilia.netlify.com/', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+  siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
+
+  siteFBAppID: '123456789', // Facebook App ID
+  userTwitter: 'emilia', // Twitter Username
+  ogSiteName: 'emilia', // Facebook Site Name
+  googleAnalyticsID: 'UA-12345689-1',
+
+  // Date format used in your project
+  // More information here: https://date-fns.org/v1.29.0/docs/format
+  dateFormat: 'DD.MM.YYYY',
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.js
+  headerFontFamily: 'Open Sans',
+  bodyFontFamily: 'Merriweather',
+  baseFontSize: '16px',
+
+  // Your information
+  avatar: '/logos/social.png',
+  name: 'jucycling',
+  location: 'Germany - Berlin',
+  socialMedia: [
+    {
+      url: 'https://www.facebook.com/prussianpedalprincess',
+      name: 'Facebook',
+    },
+    {
+      url: 'https://www.instagram.com/prussianpedalprincess',
+      name: 'Instagram',
+    },
+    {
+      url: 'http://open.spotify.com/user/af5m9k7pmg7fn9gs8k7q1azih',
+      name: 'Spotify',
+    },
+  ],
+};
+
---
+++ content/projects/2017-12-03/index.md
@@ -0,0 +1,10 @@
+---
+cover: "./roman-kraft.jpg"
+date: "2017-12-03"
+title: "Sound"
+areas:
+    - Paddy
+---
+
+![](./carlota-vidal.jpg)
+![](./tanalee-youngblood.jpg)
